3a34f2fddd026e61da6be58c343d25ccf9d9f064
Ben Chanapai <ben_chanapai04@hotmail.com>
2022-05-18 17:41:54 +0700

    Set up hover effect that changes the grid divs color

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ for (let i = 0; i < 256; i++) {
     squareDiv.classList.add('square-div');
     container.appendChild(squareDiv);
 }
+
+squareDivs = document.querySelectorAll('.square-div')
+squareDivs.forEach(div => div.addEventListener(('mouseover'), () => div.classList.add('colored')));
